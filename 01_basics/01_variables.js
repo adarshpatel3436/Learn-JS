d6ed8978c783809bb2cd61cd76efdd4c075f2264
Adarsh Patel <161011841+adarshpatel3436@users.noreply.github.com>
2025-11-26 11:27:22 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234 // can't be changed
+
+let accountEmail = "adarsh1234" // can't declare again but you can modify
+accountEmail = "adarshhh"
+
+var accountPassword = "@123" //you can change and declare
+
+accountCity = "Bhopal" // not best way
+
+let accountState; //value == undefined
+
+console.log(accountEmail)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
