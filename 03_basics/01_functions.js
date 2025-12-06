2e9410ece52f16520d44b1fd34e802fd6c1d1bdb
Adarsh Patel <161011841+adarshpatel3436@users.noreply.github.com>
2025-12-06 18:51:50 +0000

    learn more about functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -42,6 +42,42 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 
- console.log(loginUserMessage("adarsh"))   // passing parameter
- console.log(loginUserMessage())  // default parameter
+ //console.log(loginUserMessage("adarsh"))   // passing parameter
+ //console.log(loginUserMessage())  // default parameter
 
+
+
+
+ function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+ console.log(calculateCartPrice(200, 400, 500, 2000))
+// print [500,2000] array bcz 200 aur 400 val me chale gye 
+
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){     // koi bhi object pass kar sakte hai
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+    //` ` ye template literal hai ye variable ko string me convert kar deta hai aur ${} ye variable ko string me embed kar deta hai
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){   // getArray array ko accept kar raha hai
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
