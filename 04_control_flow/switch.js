16dfd16082e980f7bf14ca02390b4d819adb1845
Adarsh Patel <161011841+adarshpatel3436@users.noreply.github.com>
2025-12-22 18:59:47 +0000

    switch

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -30,3 +30,24 @@ switch (month) {
         break;
         
 }
+
+//const month = "march"
+
+//switch (month) {
+    //case "jan":
+    //    console.log("January");
+    //    break;
+   // case "feb":
+    //    console.log("feb");
+   //     break;
+   // case "march":
+     //   console.log("march");
+       // break;
+    //case "april":
+      //  console.log("april");
+        //break;
+
+    //default:
+      //  console.log("default case match");
+        //break;
+//}
