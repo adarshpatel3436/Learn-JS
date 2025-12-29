ce078d2664254bce8714b1e67e722464f6bff40b
Adarsh Patel <161011841+adarshpatel3436@users.noreply.github.com>
2025-12-29 14:25:09 +0000

    for each loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,40 @@
+const coding = ["js", "java" , "python" , "cpp" , "ruby"]
+
+//coding.forEach(function (lang) {
+  //  console.log(lang);
+//})
+
+//coding.forEach( (item) => {
+    //console.log(item);
+//})
+
+//function printme (item){
+//  console.log(item);   
+//}
+
+//coding.forEach(printme)
+
+//coding.forEach((item , index , arr) => {
+//    console.log(item , index ,arr);  
+//})
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: ".js"
+    },
+    {
+        languageName: "java",
+        languageFileName: ".java"
+    },
+    {
+        languageName: "python",
+        languageFileName: ".py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName); //, item.languageFileName);
+} )
+
